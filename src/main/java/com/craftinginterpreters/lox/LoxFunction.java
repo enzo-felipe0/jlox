a302e97df84589b274958fb8eb4f31a547268ec6
Elias66Fernandes <francisco.elias@discente.ufma.br>
2025-12-08 17:36:19 -0300

    Feat (LoxFunction, return, interpreter): implementação completa de functions

--- src/main/java/com/craftinginterpreters/lox/LoxFunction.java
+++ src/main/java/com/craftinginterpreters/lox/LoxFunction.java
@@ -4,31 +4,40 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
-    LoxFunction(Stmt.Function declaration) {
+    private final Environment closure;
+
+    LoxFunction(Stmt.Function declaration, Environment closure) {
+        this.closure = closure;
         this.declaration = declaration;
     }
-}
 
-@Override
-public String toString() {
-    return "<fn " + declaration.name.lexeme + ">";
-}
+    @Override
+    public Object call(Interpreter interpreter,
+                       List<Object> arguments) {
+        Environment environment = new Environment(closure);
+        for (int i = 0; i < declaration.params.size(); i++) {
+            environment.define(declaration.params.get(i).lexeme,
+                    arguments.get(i));
+        }
 
-
-@Override
-public int arity() {
-    return declaration.params.size();
-}
-
-@Override
-public Object call(Interpreter interpreter,
-                   List<Object> arguments) {
-    Environment environment = new Environment(interpreter.globals);
-    for (int i = 0; i < declaration.params.size(); i++) {
-        environment.define(declaration.params.get(i).lexeme,
-                arguments.get(i));
+        try {
+            interpreter.executeBlock(declaration.body, environment);
+        } catch (Return returnValue) {
+            return returnValue.value;
+        }
+        return null;
     }
 
-    interpreter.executeBlock(declaration.body, environment);
-    return null;
+    @Override
+    public int arity() {
+        return declaration.params.size();
+    }
+
+    @Override
+    public String toString() {
+        return "<fn " + declaration.name.lexeme + ">";
+    }
+
+
 }
+
