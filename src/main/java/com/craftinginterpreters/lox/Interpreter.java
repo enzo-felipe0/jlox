1952506a5fd80e0859c17bf07e703afaba2decca
enzo <enzofelipe2525@gmail.com>
2025-12-08 22:07:30 -0300

    feat: permitir propriedades em instâncias.

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -56,6 +56,20 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Object visitSetExpr(Expr.Set expr) {
+        Object object = evaluate(expr.object);
+
+        if (!(object instanceof LoxInstance)) {
+            throw new RuntimeError(expr.name,
+                    "Only instances have fields.");
+        }
+
+        Object value = evaluate(expr.value);
+        ((LoxInstance)object).set(expr.name, value);
+        return value;
+    }
+
+    @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
 
@@ -285,6 +299,17 @@ class Interpreter implements Expr.Visitor<Object>,
         }
 
         return function.call(this, arguments);
+    }
+
+    @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        Object object = evaluate(expr.object);
+        if (object instanceof LoxInstance) {
+            return ((LoxInstance) object).get(expr.name);
+        }
+
+        throw new RuntimeError(expr.name,
+                "Only instances have properties.");
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
